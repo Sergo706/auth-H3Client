fc7251fbe23f86e737081f8a589bc1ff040e0cea
Sergo706 <187537278+Sergo706@users.noreply.github.com>
2025-09-26 05:14:23 +0300

    Fix clockTolerance parameter placement in verifyOAuthToken function for clarity

--- src/utils/verifyOAuthTokens.ts
+++ src/utils/verifyOAuthTokens.ts
@@ -6,6 +6,7 @@ export async function verifyOAthToken(id_token: string, url: string, issuer: str
   const { payload } = await jwtVerify(id_token, JWKS, {
     issuer: issuer,
     audience: clientId,
+    clockTolerance: 5
   })
     const userInfo = payload;
     return userInfo;
