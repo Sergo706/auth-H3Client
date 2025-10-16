7c0d5796a11f9c4ddaa94a798af33b6488ef6c14
Sergo706 <187537278+Sergo706@users.noreply.github.com>
2025-10-16 05:59:02 +0300

    trying to fix the nitro integration.

--- src/nitroPluginStarter.ts
+++ src/nitroPluginStarter.ts
@@ -1,4 +1,4 @@
-import { H3 } from 'h3'
+import { createApp, createRouter } from 'h3'
 import  csrfToken  from './middleware/csrf.js';
 import { configuration } from './config/config.js'
 import {validator} from './middleware/visitorValid.js'
@@ -8,27 +8,31 @@ import {useAuthRoutes} from './routes/auth.js';
 import {magicLinksRouter} from './routes/magicLinks.js';
 import { useOAuthRoutes } from './routes/OAuth.js';
 import type { Configuration }  from "./types/configSchema.js"
-
+import { nitroShim } from './middleware/nitroShim.js';
 
 export async function startService(config: Configuration) {
     configuration(config)
     console.log('auth called')
-    const app = new H3();
-
+    const app = createApp();
+    const router = createRouter();
+    app.use(nitroShim);
     app.register(httpLogger());
     app.use(isValidIP)
     app.use(validator)
     app.use(csrfToken)
 
-    useAuthRoutes(app)
-    magicLinksRouter(app)
-    useOAuthRoutes(app)
+    useAuthRoutes(router)
+    magicLinksRouter(router)
+    useOAuthRoutes(router)
+
     app.get(`/test`, (event) => {
         event.res.status = 200
         return {msg: 'IT WORKS'}
     })
+
     return {
         handler: app.handler,
-        fetchHandler: app.fetch
+        fetchHandler: app.fetch,
+        app: app
     };
 }
