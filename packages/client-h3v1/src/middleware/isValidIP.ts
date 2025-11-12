faac71e5b9ca0a80026338a6430ae3d300df6c25
Sergo706 <187537278+Sergo706@users.noreply.github.com>
2025-11-12 19:12:07 +0200

    chore: modified error handling for isValidIp middleware.

--- packages/client-h3v1/src/middleware/isValidIP.ts
+++ packages/client-h3v1/src/middleware/isValidIP.ts
@@ -11,9 +11,19 @@ import { getLogger } from "../utils/logger.js";
  * @throws HTTPError When the IP is missing or malformed.
  */
 export default defineEventHandler((event) => {
-  const ipAddress = getRequestIP(event)
+  const ipAddress = getRequestIP(event) ?? getRequestIP(event, { xForwardedFor: true }) ??
+                    event.node.req.headers['x-real-ip'] as string | undefined;
+                    
   const log = getLogger().child({service: 'auth-client', branch: 'entry', type: 'middleware'})
-    if (!ipAddress || isIP(ipAddress) === 0) {
+
+      if (!ipAddress) {
+      throwError(log,event,'FORBIDDEN', 403, 'Forbidden', 'BAD_CLIENT', `
+        No ipAddress is provided.
+        ${ipAddress}
+        `)
+    }
+
+    if (isIP(ipAddress) === 0) {
       throwError(log, event, 'AUTH_SERVER_ERROR', 403, 'Forbidden', 'BAD_CLIENT', `
         Bad client ip address ${ipAddress}
         `)
