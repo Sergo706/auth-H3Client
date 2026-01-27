b68b8723c1c52b5cad2fe2ba5d350b46c373c7b0
Sergo706 <187537278+Sergo706@users.noreply.github.com>
2026-01-27 02:09:41 +0200

    added executeRequest to client pkg

--- packages/client/main.ts
+++ packages/client/main.ts
@@ -1,3 +1,4 @@
 export { useAuthData, type AuthState } from "./composables/useAuthData.js";
+export { executeRequest } from './utils/executeRequest.js';
 export { getCsrfToken } from "./utils/getCsrfToken.js";
 export { type ServerResponse } from '@internal/shared';
