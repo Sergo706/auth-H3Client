d535db51b9d0a84f3c1ea56c21ce72134cade994
Sergo706 <187537278+Sergo706@users.noreply.github.com>
2026-01-27 02:23:02 +0200

    added executeRequest to client pkg

---
+++ packages/client/utils/executeRequest.ts
@@ -0,0 +1,85 @@
+import { $fetch, FetchOptions } from "ofetch";
+import { getCsrfToken } from "./getCsrfToken.js";
+import { appendResponseHeader, type H3Event } from "auth-h3client/v1";
+import { type Results } from "@internal/shared";
+import { useNuxtApp, useRequestHeaders } from "nuxt/app";
+
+export async function executeRequest<T>(
+    url: string,
+    method: "GET" | "POST" | "DELETE" | "PUT", 
+    body?: object, 
+    customHeaders: Record<string, string> = {},
+): Promise<Results<T>> {
+    try {
+        const dataType = method === "GET" ? {query: body} : {body: body};
+        let token;
+        
+        if (import.meta.client) {
+             token = getCsrfToken();
+        }
+        
+
+        const headers: Record<string, string> = {
+            ...customHeaders,
+            'X-Forwarded-For': customHeaders['x-forwarded-for'] ?? '127.0.0.1',
+        };
+
+        if (import.meta.client && token) {
+            headers['X-CSRF-Token'] = token;
+        }
+
+      
+        if (import.meta.server) {
+            const reqHeaders = useRequestHeaders();
+            Object.assign(headers, reqHeaders);
+        }
+        
+        const results = await $fetch.raw<Results<T>>(url, {
+            method,
+            timeout: 15000,
+            ignoreResponseError: true,
+            ...dataType,
+            headers,
+        });
+
+        if (import.meta.server) {
+            const cookies = results.headers.getSetCookie();
+            if (cookies && cookies.length > 0) {
+                const nuxtApp = useNuxtApp();
+                const event = nuxtApp.ssrContext?.event as unknown as H3Event;
+                if (event) {
+                    appendResponseHeader(event, 'set-cookie', cookies);
+                }
+            }
+        }
+
+        const json = results._data;
+
+        if (!json) return { 
+             ok: false,
+             date: new Date().toISOString(),
+            reason: 'Server Error. Empty response.' 
+        };
+
+        if (!results.ok || results.status !== 200) {
+             return { 
+                ok: false,
+                date: new Date().toISOString(), 
+                reason: 'Server Error. Status not 200.' 
+            };
+        }
+
+        if ('ok' in json && !json.ok) {
+             return json as Results<T>;
+        }
+
+        return json;
+
+    } catch (err) {
+        return {
+            ok: false,
+            date: new Date().toISOString(),
+            reason: err instanceof Error ? err.message : JSON.stringify(err)
+        };
+    }
+}
