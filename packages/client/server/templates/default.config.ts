06695e084c0502a7fa09c167b9e5b7a54869275c
Sergo706 <187537278+Sergo706@users.noreply.github.com>
2026-02-01 11:24:54 +0200

    feat: Introduce server-side configuration templates for default and OAuth setups, along with updated documentation and build configurations.

---
+++ packages/client/server/templates/default.config.ts
@@ -0,0 +1,40 @@
+import type { Configuration } from "@internal/shared";
+import { useStorage } from "nitropack/runtime/storage";
+import crypto from 'node:crypto'
+
+const random = () => crypto.randomBytes(32).toString('hex');
+const required = (name: string): string => {
+    throw new Error(`Missing required env var: ${name}`);
+};
+export const configDefaults: Configuration = {
+    server: {
+        auth_location: {
+            serverOrDNS: process.env.AUTH_SERVER_LOCATION || required('AUTH_SERVER_LOCATION'),
+            port: Number(process.env.AUTH_PORT_LOCATION || required('AUTH_PORT_LOCATION'))
+        },
+        hmac: {
+            enableHmac: true as const,
+            clientId: process.env.HMAC_CLIENT_ID ?? random(),
+            sharedSecret: process.env.HMAC_SHARED_SECRET ?? random(),
+        },
+
+    ssl: {
+        enableSSL: false as const,
+    },
+
+    cryptoCookiesSecret: process.env.AUTH_CRYPTO_COOKIES ?? random(),
+    },
+    uStorage: {
+        storage: useStorage('cache'),
+        cacheOptions: {
+                successTtl: 60 * 60 * 24 * 30, 
+                rateLimitTtl: 10    
+        }
+    },
+    onSuccessRedirect: `/`,
+    enableFireWallBans: false,
+    telegram: {
+        enableTelegramLogger: false as const,
+    },
+    logLevel: 'info' as const
+}
