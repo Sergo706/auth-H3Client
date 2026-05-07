c28a15ed1195d0613d587a32865db5fa279f141b
Sergo706 <187537278+Sergo706@users.noreply.github.com>
2026-05-07 06:28:36 +0300

    fix: removed types exports from internal

--- packages/client-h3v2/src/main.ts
+++ packages/client-h3v2/src/main.ts
@@ -70,5 +70,4 @@ export { defineAuthenticatePublicApi } from './utils/verifyWrapper.js'
 export * from './utils/apiPostWrapper.js'
 export { default as getApiListsController } from './controllers/apiGetLists.js'
 export { newApiTokenSchema, privilegeQ, reqParams, ipRestrictionUpdate, tokenId} from "@internal/shared";
-export type * from '@internal/shared';
 export { defineByteLimiterHandler } from "./utils/limitBytesWrapper.js"
